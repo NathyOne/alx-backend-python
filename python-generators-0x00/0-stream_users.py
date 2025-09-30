b6c9e7dffb92a6a237a27a8c815c9e51b9476972
NathyOne <nathanielabebe32@gmail.com>
2025-09-30 11:41:52 +0300

    streaming using generator

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+
+def stream_rows(query, host='localhost', user='root', password='', database=''):
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        buffered=False,
+    )
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(query)
+
+    while True:
+        rows = cursor.fetchmany(6)
+        if not rows:
+            break
+        for row in rows:
+            yield row
+
+    cursor.close()
+    connection.close()
+
+
+for row in stream_rows(
+    query="SELECT * FROM user_data LIMIT 6",
+    host="localhost",
+    user="root",
+    password="root",
+    database="ALX_prodev"
+):
+    print(f"Name: {row['name']}, Email: {row['email']}")
